5cb7d01a0ed61bde1840caee5fe039c435e9d344
rashidul191 <67516291+rashidul191@users.noreply.github.com>
2020-10-03 14:03:50 +0600

    add header and home component

---
+++ src/component/Header/Header.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import './Header.css';
+import { Button, Form, FormControl, Nav, Navbar } from 'react-bootstrap';
+
+const Header = () => {
+    return (
+        <div className="bg-img">
+            <Navbar bg="" variant="light">
+                <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+                <Nav className="ml-auto">
+                <Nav.Link href="#home">Home</Nav.Link>
+                <Nav.Link href="#donation">Donation</Nav.Link>
+                <Nav.Link href="#events">Events</Nav.Link>
+                <Nav.Link href="#Blog">Blog</Nav.Link>
+                <Button variant="primary">Register</Button>
+                <Button variant="dark">Admin</Button>
+                </Nav>
+               
+            </Navbar>
+                <h1 className="text-center">I GROW BY HELPING PEOPLE IN NEED.</h1>
+            <Form inline>
+                    <FormControl type="text" placeholder="Search" className=" " />
+                    <Button variant="primary">Search</Button>
+                </Form>
+            
+        </div>
+    );
+};
+
+export default Header;
